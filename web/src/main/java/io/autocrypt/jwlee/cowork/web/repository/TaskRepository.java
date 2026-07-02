05e603c31fc2a5c0c381d7ec7bf6d03b3c4c6b0b
Jinwoo Lee <woojung3@postech.ac.kr>
2026-07-02 10:23:22 +0900

    deleted duplicated web directory

--- web/src/main/java/io/autocrypt/jwlee/cowork/web/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package io.autocrypt.jwlee.cowork.web.repository;
-
-import io.autocrypt.jwlee.cowork.web.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface TaskRepository extends JpaRepository<Task, String> {
-    List<Task> findByUserIdOrderByCreatedAtDesc(String userId);
-}
